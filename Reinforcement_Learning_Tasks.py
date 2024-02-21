fdf417a3cc8e5ab7a8ef94cfbd11bf0168527538
Harshit Dawar <harshit.dawar55@gmail.com>
2024-02-21 15:44:45 +0530

    Functions used in the RL

--- Reinforcement_Learning_Tasks.py
+++ Reinforcement_Learning_Tasks.py
@@ -1,9 +1,10 @@
-from typing import Tuple, Dict, Optional, Iterable
+from typing import Tuple, Dict, Optional, Iterable, Callable
 
 import numpy as np
+import seaborn as sns
 import matplotlib
 from matplotlib import animation
-
+import matplotlib.pyplot as plt
 from IPython.display import HTML
 
 import gym
@@ -172,6 +173,33 @@ class Maze(gym.Env):
         return distances
 
 
+def plot_policy(probs_or_qvals, frame, action_meanings=None):
+    if action_meanings is None:
+        action_meanings = {0: 'U', 1: 'R', 2: 'D', 3: 'L'}
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    max_prob_actions = probs_or_qvals.argmax(axis=-1)
+    probs_copy = max_prob_actions.copy().astype(object)
+    for key in action_meanings:
+        probs_copy[probs_copy == key] = action_meanings[key]
+    sns.heatmap(max_prob_actions, annot=probs_copy, fmt='', cbar=False, cmap='coolwarm',
+                annot_kws={'weight': 'bold', 'size': 12}, linewidths=2, ax=axes[0])
+    axes[1].imshow(frame)
+    axes[0].axis('off')
+    axes[1].axis('off')
+    plt.suptitle("Policy", size=18)
+    plt.tight_layout()
+
+
+def plot_values(state_values, frame):
+    f, axes = plt.subplots(1, 2, figsize=(10, 4))
+    sns.heatmap(state_values, annot=True, fmt=".2f", cmap='coolwarm',
+                annot_kws={'weight': 'bold', 'size': 12}, linewidths=2, ax=axes[0])
+    axes[1].imshow(frame)
+    axes[0].axis('off')
+    axes[1].axis('off')
+    plt.tight_layout()
+
+
 def display_video(frames):
     # Copied from: https://colab.research.google.com/github/deepmind/dm_control/blob/master/tutorial.ipynb
     orig_backend = matplotlib.get_backend()
@@ -188,3 +216,25 @@ def display_video(frames):
     anim = animation.FuncAnimation(fig=fig, func=update, frames=frames,
                                     interval=50, blit=True, repeat=False)
     return HTML(anim.to_html5_video())
+
+
+def test_agent(env, policy, episodes=10):
+    frames = []
+    for episode in range(episodes):
+        state = env.reset()
+        done = False
+        frames.append(env.render(mode="rgb_array"))
+
+        while not done:
+            p = policy(state)
+            if isinstance(p, np.ndarray):
+                action = np.random.choice(4, p=p)
+            else:
+                action = p
+            next_state, reward, done, extra_info = env.step(action)
+            img = env.render(mode="rgb_array")
+            frames.append(img)
+            state = next_state
+
+    return display_video(frames)
+
